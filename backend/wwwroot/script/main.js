13f10dbbc5806943ac5e2ba61db77e9121e7c401
Thomas Jung <thomas.jung6@de.bosch.com>
2023-05-09 15:31:33 +0200

    Landkarte geht!

--- backend/wwwroot/script/main.js
+++ backend/wwwroot/script/main.js
@@ -1,8 +1,37 @@
 async function initialize() 
 {
-    document.body.textContent = "Nope"
-
     var response = await fetch('https://localhost:7010/api/Nodes');
     var data = await response.json();
-    console.log(data);    
+
+    let canv = document.getElementById("canvas"); 
+    var ctx = canv.getContext("2d");
+
+    for(let i = 0; i < data.length; i++) {
+        let obj = data[i];
+
+        if (obj.name.includes('WILNSDORF'))
+            drawFilledCircle(ctx, obj.coordinates.x, obj.coordinates.y, 5, 'red'); 
+        else if (obj.name.includes('HERRENBERG'))
+            drawFilledCircle(ctx, obj.coordinates.x, obj.coordinates.y,5, 'red'); 
+        else 
+            drawFilledCircle(ctx, obj.coordinates.x, obj.coordinates.y, 1, 'black'); 
+            
+    }
+
+    canv.a
+}
+
+function drawFilledCircle(ctx, x, y, size, color)
+{
+    ctx.beginPath(); // Beginnen Sie einen neuen Zeichenpfad
+
+    x = 85 * (x - 5.0); 
+
+    y = 130 * (y - 47); 
+    y = 1000 - y; 
+
+    ctx.arc(x, y, size, 0, 2 * Math.PI, false);
+    ctx.fillStyle = color; 
+    ctx.fill(); // Füllen Sie den Kreis mit der zuvor festgelegten Farbe
+    
 }
